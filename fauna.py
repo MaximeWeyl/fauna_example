727d0bd9b98df44c701a3ffe5762ebbb5fed3d49
Maxime Weyl <maxime.weyl@ovhcloud.com>
2022-06-08 13:53:01 +0200

    simplifies the configuration loader

--- fauna.py
+++ fauna.py
@@ -1,23 +1,9 @@
 from faunadb.client import FaunaClient
+from dynaconf import LazySettings
 
-
-from dynaconf import Dynaconf
-from dynaconf.constants import EXTS
-
-DEFAULT_SETTINGS_FILES = [f"config.{ext}" for ext in EXTS] + [
-    f".secrets.{ext}" for ext in EXTS
-]
-
-config =  Dynaconf(
-    # This global `settings` is deprecated from v3.0.0+
-    # kept here for backwards compatibility
-    # To Be Removed in 4.0.x
-    warn_dynaconf_global_settings=True,
+config = LazySettings(
     environments=True,
-    lowercase_read=False,
-    load_dotenv=True,
-    settings_files=DEFAULT_SETTINGS_FILES,
-    envvar_prefix='NOUNOUTOP',
+    settings_files=["config.toml"],
 )
 
 
